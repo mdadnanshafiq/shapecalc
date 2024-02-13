99d52d01165b855c0ba8ca4a734612e7b47ed32f
mdadnanshafiq <adnanbgic2023@gmail.com>
2024-02-13 17:10:03 +0600

    area done

--- script.js
+++ script.js
@@ -9,10 +9,9 @@ triBtn.addEventListener('click', function () {
     let calc = 0.5 * values[0] * values[1];
 
     triResult.innerText = calc;
-    
-    let li = document.createElement('li');
-    li.innerText = document.getElementById('triShow').innerText;
-    resultList.appendChild(li);
+    let inner = 'triShow';
+    let outer = 'triTemp';
+    areaShow(inner, outer);
 })
 
 function getFloat(a, b) {
@@ -25,6 +24,19 @@ function getFloat(a, b) {
     values.push(value2Float);
     return values;
 }
+
+function areaShow(a, b) {
+    let li = document.createElement('li');
+    li.innerText = document.getElementById(a).innerText;
+    li.classList.add(b);
+    let c = '.'+b;
+    let remove = resultList.querySelectorAll(c);
+    remove.forEach(i => {
+        i.remove();
+    });
+    resultList.appendChild(li);
+}
+
 
 let bgColor = document.getElementById('bgColor');
 

a9480c2dbda26f78c5859521f3a88b64ad0c4b66
mdadnanshafiq <adnanbgic2023@gmail.com>
2024-02-13 16:50:37 +0600

    triangle done

---
+++ script.js
@@ -0,0 +1,33 @@
+let triBase = document.getElementById('triBase');
+let triHeight = document.getElementById('triHeight');
+let triResult = document.getElementById('triResult');
+let triBtn = document.getElementById('triBtn');
+let resultList = document.getElementById('resultList');
+
+triBtn.addEventListener('click', function () {
+    let values = getFloat(triBase, triHeight);
+    let calc = 0.5 * values[0] * values[1];
+
+    triResult.innerText = calc;
+    
+    let li = document.createElement('li');
+    li.innerText = document.getElementById('triShow').innerText;
+    resultList.appendChild(li);
+})
+
+function getFloat(a, b) {
+    let value1 = a.value;
+    let value2 = b.value;
+    let value1Float = parseFloat(value1);
+    let value2Float = parseFloat(value2);
+    let values = [];
+    values.push(value1Float);
+    values.push(value2Float);
+    return values;
+}
+
+let bgColor = document.getElementById('bgColor');
+
+bgColor.addEventListener('mouseover', function () {
+    this.style.backgroundColor = '#fff1ff';
+})
